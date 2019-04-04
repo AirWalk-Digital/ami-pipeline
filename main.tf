0c63142181e9dda00d9ae13b5372a8a5dda58144
Mikolaj Nowacki <mikolaj.nowacki@gmail.com>
2019-04-04 10:20:47 +0100

    Provision instance with ssm run command

--- main.tf
+++ main.tf
@@ -34,6 +34,7 @@ data "template_file" "ssm_automation_document" {
   vars = {
     subnet               = "${module.vpc.private_subnet_ids[0]}"
     lambda_function_name = "${module.ami_pipeline_function.lambda_function["name"]}"
+    instance_profile     = "${aws_iam_instance_profile.instance_profile.arn}"
   }
 }
 
@@ -46,7 +47,7 @@ resource "aws_ssm_document" "ami_pipeline" {
 }
 
 resource "aws_launch_template" "launch_template" {
-  name_prefix   = "foobar"
+  name_prefix   = "launch-template"
   image_id      = "ami-08d658f84a6d84a80"
   instance_type = "t2.micro"
 }
@@ -66,4 +67,37 @@ resource "aws_autoscaling_group" "autoscaling_group" {
 module "ami_pipeline_function" {
   source = "modules/lambda"
   name   = "ami_pipeline"
+}
+
+resource "aws_iam_instance_profile" "instance_profile" {
+  name = "ami-pipeline-instance-profile"
+  role = "${aws_iam_role.role.name}"
+}
+
+resource "aws_iam_role" "role" {
+  name = "ami-pipeline-instance-role"
+  path = "/"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+               "Service": "ec2.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+EOF
+}
+
+# ideally we shouldn't grant full access to ssm
+resource "aws_iam_policy_attachment" "main" {
+  name       = "ami-pipeline-instance-role"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMFullAccess"
+  roles      = ["${aws_iam_role.role.name}"]
 }
